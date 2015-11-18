696b4fcd5861a17debf023204b6750f2d997b5a1
Whyssky <rennancge@hotmail.com>
2015-11-18 14:26:17 -0300

    Update ArenaManager

--- src/me/whyssky/ctf/Manager/ArenaManager.java
+++ src/me/whyssky/ctf/Manager/ArenaManager.java
@@ -1,10 +1,15 @@
 package me.whyssky.ctf.Manager;
 
 import java.io.File;
+import java.util.ArrayList;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
+import me.whyssky.ctf.Arena;
 import me.whyssky.ctf.CTF;
 
 public class ArenaManager {
@@ -13,8 +18,13 @@ public class ArenaManager {
 	private CTF plugin = CTF.instance;
 	private FileConfiguration getConfig = CTF.instance.getConfig();
 	private File afile;
+	private FileConfiguration arenasf;
 
 	private Location lobby;
+	
+	// Arenas
+    private ArrayList<Arena> arenas = new ArrayList<>();
+    public static final ArrayList<Integer> ids = new ArrayList<>();
 	
 	//Prevent instantiation
 	private ArenaManager() {}
@@ -26,6 +36,34 @@ public class ArenaManager {
 				afile.createNewFile();
 			}catch(Exception e) {}
 		}
+		
+		arenasf = YamlConfiguration.loadConfiguration(afile);
+		
+		 try {
+			 for (int i : arenasf.getIntegerList("ids")) {
+				 if(!ids.contains(i)) {
+					 ids.add(i);
+	             }
+				 
+				 Arena a = new Arena(i);
+				 
+				 arenas.add(a);
+				 
+				 plugin.saveConfig();
+				 plugin.reloadConfig();
+			 }
+	            
+			 if(arenasf.getConfigurationSection("lobby") != null) {
+				 World world = Bukkit.getServer().getWorld(arenasf.getConfigurationSection("lobby").getString("world"));
+				 int x = (int) arenasf.getConfigurationSection("lobby").getDouble("x");
+				 int y = (int) arenasf.getConfigurationSection("lobby").getDouble("y");
+				 int z = (int) arenasf.getConfigurationSection("lobby").getDouble("z");
+				 Location l = new Location(world, x, y, z);
+
+				 this.lobby = l;
+			 }
+	            
+		}catch (Exception ignored) {}
 	}
 	
 	public static ArenaManager getManager() {
